d681233b2755a29b2ea01ff29bbe73dacce6fce0
tyange <usun16@gmail.com>
2024-11-30 18:34:20 +0900

    shuffle

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
+use rand::{seq::SliceRandom, thread_rng};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        Deck { cards }
     }
 
-    let deck = Deck { cards };
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
 
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+    // Probably need to add error handling!!
+    let cards = deck.deal(3);
+
+    println!("Heres your hand: {:#?}", cards);
     println!("Heres your deck: {:#?}", deck);
 }
